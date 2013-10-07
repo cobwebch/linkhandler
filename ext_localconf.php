3469f8b0a741c1a15576a9074d58b2f2b1bd5888
Alexander Stehlik <alexander.stehlik@googlemail.com>
2013-10-07 20:10:04 +0200

    [TASK] Improved tt_news TSConfig inclusion
    The addPageTSConfig() was moved to the correct place (ext_localconf.php)
    and it will only be included if the new Extension configration option
    includeTtNewsTsConfig is set to TRUE.

--- ext_localconf.php
+++ ext_localconf.php
@@ -10,4 +10,23 @@ $GLOBALS['TYPO3_CONF_VARS']['SC_OPTIONS']['tslib/class.tslib_content.php']['typo
 $GLOBALS['TYPO3_CONF_VARS']['SC_OPTIONS']['ext/rtehtmlarea/mod3/class.tx_rtehtmlarea_browse_links.php']['browseLinksHook'][] = 'Aoe\\Linkhandler\\Browser\\ElementBrowserHook';
 $GLOBALS['TYPO3_CONF_VARS']['SC_OPTIONS']['typo3/class.browse_links.php']['browseLinksHook'][] = 'Aoe\\Linkhandler\\Browser\\ElementBrowserHook';
 
-?>
+$linkhandlerExtConf = unserialize($GLOBALS['TYPO3_CONF_VARS']['EXT']['extConf']['linkhandler']);
+
+if (is_array($linkhandlerExtConf) && $linkhandlerExtConf['includeTtNewsTsConfig']) {
+
+	\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addPageTSConfig('
+		RTE.default.tx_linkhandler {
+			tt_news {
+				label = News
+				listTables = tt_news
+			}
+		}
+
+		mod.tx_linkhandler {
+			tt_news {
+				label = News
+				listTables = tt_news
+			}
+		}
+	');
+}
--- ext_tables.php
+++ ext_tables.php
@@ -4,19 +4,3 @@ if (!defined('TYPO3_MODE')) {
 }
 
 \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addStaticFile($_EXTKEY, 'Configuration/TypoScript/', 'Link handler');
-
-\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addPageTSConfig('
-RTE.default.tx_linkhandler {
-	tt_news {
-		label=News
-		listTables=tt_news
-	}
-}
-
-mod.tx_linkhandler {
-	tt_news {
-		label=News
-		listTables=tt_news
-	}
-}
-');
