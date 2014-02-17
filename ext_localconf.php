41d29fdf30e46fa6d101af232fd1c51a9b8cda30
Alexander Stehlik <alexander.stehlik@googlemail.com>
2014-02-17 21:04:40 +0100

    [BUGFIX] Prevent data-htmlarea-external link attribute
    A hook was implemented for the RteHtmlParser class to
    prevent the attribute from being re-inserted by the parser because
    it does not recognize the link type.

--- ext_localconf.php
+++ ext_localconf.php
@@ -8,6 +8,7 @@ $GLOBALS['TYPO3_CONF_VARS']['SC_OPTIONS']['tslib/class.tslib_content.php']['typo
 
 //register hook
 $GLOBALS['TYPO3_CONF_VARS']['SC_OPTIONS']['ext/rtehtmlarea/mod3/class.tx_rtehtmlarea_browse_links.php']['browseLinksHook'][] = 'Aoe\\Linkhandler\\Browser\\ElementBrowserHook';
+$GLOBALS['TYPO3_CONF_VARS']['SC_OPTIONS']['t3lib/class.t3lib_parsehtml_proc.php']['modifyParams_LinksRte_PostProc'][] = 'Aoe\\Linkhandler\\RteParserHook';
 $GLOBALS['TYPO3_CONF_VARS']['SC_OPTIONS']['typo3/class.browse_links.php']['browseLinksHook'][] = 'Aoe\\Linkhandler\\Browser\\ElementBrowserHook';
 
 $linkhandlerExtConf = unserialize($GLOBALS['TYPO3_CONF_VARS']['EXT']['extConf']['linkhandler']);
