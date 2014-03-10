31de6fc89ad7c02e2ded43c77077162230ef2bc5
Alexander Stehlik <alexander.stehlik@googlemail.com>
2014-03-10 18:56:03 +0100

    [TASK] Added ext_autoload.php
    This should make the the Extension compatible with TYPO3 6.1.

--- ext_emconf.php
+++ ext_emconf.php
@@ -35,7 +35,7 @@ $EM_CONF[$_EXTKEY] = array(
 	'constraints' => array(
 		'depends' => array(
 			'php' => '5.3.0-0.0.0',
-			'typo3' => '6.0.0-6.0.99',
+			'typo3' => '6.1.0-6.2.99',
 		),
 		'conflicts' => array(
 			'ch_rterecords',
