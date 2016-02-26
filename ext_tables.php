5e76fe93571af0fa2de083dd57e8b25a04e44395
Francois Suter <francois@typo3.org>
2016-02-26 10:31:12 +0100

    [BUGFIX] Restore inclusion of static templates
    Restore abusively deleted statement declaring static TS
    template files.

--- ext_tables.php
+++ ext_tables.php
@@ -14,3 +14,20 @@ if (!defined('TYPO3_MODE')) {
     'Configuration/TSconfig/Page/tt_news.ts',
     'EXT:linkhandler - Configuration for "tt_news"'
 );
+
+// Register static TypoScript templates
+if (\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::isLoaded('tt_news')) {
+	\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addStaticFile(
+            'linkhandler',
+            'Configuration/TypoScript/tt_news',
+            'Link handler - tt_news'
+    );
+}
+
+if (\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::isLoaded('news')) {
+	\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addStaticFile(
+            'linkhandler',
+            'Configuration/TypoScript/news',
+            'Link handler - news'
+    );
+}
