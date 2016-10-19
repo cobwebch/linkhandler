7c0ab682a6901408305d63e1d846ae492eb66a23
Markus Klein <markus.klein@reelworx.at>
2016-10-19 03:37:48 +0200

    [CLEANUP] Extract TCA manipulations to overrides

--- ext_tables.php
+++ ext_tables.php
@@ -1,35 +1,5 @@
 <?php
-if (!defined('TYPO3_MODE')) {
-    die ('Access denied.');
-}
-
-// Register Page TSconfig for inclusion
-\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::registerPageTSConfigFile(
-    'linkhandler',
-    'Configuration/TSconfig/Page/news.ts',
-    'EXT:linkhandler - Configuration for "news"'
-);
-\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::registerPageTSConfigFile(
-    'linkhandler',
-    'Configuration/TSconfig/Page/tt_news.ts',
-    'EXT:linkhandler - Configuration for "tt_news"'
-);
-
-// Register static TypoScript templates
-if (\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::isLoaded('tt_news')) {
-    \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addStaticFile(
-        'linkhandler',
-        'Configuration/TypoScript/tt_news',
-        'Link handler - tt_news'
-    );
-}
-if (\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::isLoaded('news')) {
-    \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addStaticFile(
-        'linkhandler',
-        'Configuration/TypoScript/news',
-        'Link handler - news'
-    );
-}
+defined('TYPO3_MODE') || die();
 
 // Add tx_linkhandler type to linkvalidator
 \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addPageTSConfig(
